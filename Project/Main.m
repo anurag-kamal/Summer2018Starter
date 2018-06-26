9ed8e3b2da9d297b3b6964835699062a119ea51a
Anurag Kamal <akamal@mtu.edu>
2018-06-26 09:26:17 -0400

    first_check_commit
    trial if gitdesktop synced properly

--- Project/Main.m
+++ Project/Main.m
@@ -1,6 +1,8 @@
+(* ::Package:: *)
+
 Package["Project`"]
 
-(* this is declaring that a certain simbol can be used IN the paclet *)
+(* this is declaring that a certain simbol can be used IN the paclet **a change*)
 PackageScope[$SomeSymbol]
 
 $SomeSymbol = 10
